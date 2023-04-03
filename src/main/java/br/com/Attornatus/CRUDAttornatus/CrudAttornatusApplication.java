bed853888a99d8e9a1c9603ad9f7b05d102cd9a9
thiago514 <thiagogogola9@gmail.com>
2023-04-03 13:42:14 -0300

    initial project

---
+++ src/main/java/br/com/Attornatus/CRUDAttornatus/CrudAttornatusApplication.java
@@ -0,0 +1,13 @@
+package br.com.Attornatus.CRUDAttornatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAttornatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAttornatusApplication.class, args);
+	}
+
+}
